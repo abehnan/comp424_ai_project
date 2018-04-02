f3d4d53e1967ac0cca2fe77753a156259e1bcb18
abehnan <abehnan@gmail.com>
2018-04-02 15:08:13 -0400

    swede v1

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,307 @@
 package student_player;
 
-public class MyTools {
-    public static double getSomething() {
-        return Math.random();
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import boardgame.Move;
+import coordinates.Coord;
+import coordinates.Coordinates;
+import javafx.scene.control.Tab;
+import tablut.TablutBoardState;
+import tablut.TablutMove;
+
+class MyTools {
+
+    // todo: add values for vulnerable positions. e.g. lose 1 point for every vulnerable piece
+    // todo: bonus for moving the a piece into a position where it can attach the king
+    // todo: make sure bonus for king distance gives value based on distance improvement on swede turn
+    // todo: decrease value if we're sacrificing a piece
+    // todo: discard move if we put our king in peril
+    // todo: seperate king move nad non king move for swedes
+    private final int player_id;
+    private final Coord CENTER = Coordinates.get(4, 4);
+    private final List<Coord> CORNERS = Coordinates.getCorners();
+    private final List<Coord> CENTER_NEIGHBOURS = Coordinates.getNeighbors(CENTER);
+
+
+    MyTools(int myPlayer) {
+        this.player_id = myPlayer;
+    }
+
+    private int muscoviteEvalBoard(TablutBoardState initialBoardState,
+                                   TablutBoardState finalBoardState,
+                                   TablutMove muscoviteMove) {
+
+        int PIECE_VALUE = 100;
+        int CORNER_CAPTURE_BONUS = 200;
+        int CENTER_CAPTURE_BONUS = 500;
+        int opponent = 1 - player_id;
+        int moveValue = 0;
+        int numPlayerPieces = finalBoardState.getNumberPlayerPieces(player_id);
+        int numOpponentPieces = finalBoardState.getNumberPlayerPieces(opponent);
+        Coord endPosition = muscoviteMove.getEndPosition();
+
+        // increase value for each piece owned, decrease for each piece owned by opponent
+        moveValue += numPlayerPieces * PIECE_VALUE;
+        moveValue -= numOpponentPieces * PIECE_VALUE;
+
+        // give additional incentive for captures at vulnerable positions
+        // encourages AI to be more aggressive
+        for (Coord centerNeighbor : CENTER_NEIGHBOURS) {
+            if (!initialBoardState.getKingPosition().equals(centerNeighbor)) {
+                if (initialBoardState.isOpponentPieceAt(centerNeighbor)) {
+                    try {
+                        Coord sandwichCoord = Coordinates.getSandwichCoord(CENTER, centerNeighbor);
+                        if (endPosition.equals(sandwichCoord)) {
+                            moveValue += CENTER_CAPTURE_BONUS;
+                        }
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        }
+        for (Coord corner : CORNERS) {
+            List<Coord> cornerNeighbours = Coordinates.getNeighbors(corner);
+            for (Coord cornerNeighbour : cornerNeighbours) {
+                if (initialBoardState.isOpponentPieceAt(cornerNeighbour)) {
+                    try {
+                        Coord sandwichCoord = Coordinates.getSandwichCoord(corner, cornerNeighbour);
+                        if (endPosition.equals(sandwichCoord)) {
+
+                            moveValue += CORNER_CAPTURE_BONUS;
+                        }
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        }
+        return moveValue;
+    }
+
+
+    private int swedeEvalBoard(TablutBoardState boardState) {
+
+        int pieceValue = 200;
+        int kingDistanceValue = 5;
+//        int vulnerableKingPenalty = 5000;
+        int opponent = 1 - player_id;
+        int moveValue = 0;
+        int numPlayerPieces = boardState.getNumberPlayerPieces(player_id);
+        int numOpponentPieces = boardState.getNumberPlayerPieces(opponent);
+
+        // increase value for each piece owned, decrease for each piece owned by opponent
+        moveValue += numPlayerPieces * pieceValue;
+        moveValue -= numOpponentPieces * pieceValue;
+
+//        // decrease value heavily if we're putting our piece in peril
+//        if (isKingVulnerable(boardState)) {
+//            moveValue -= vulnerableKingPenalty;
+//        }
+
+        int kingSafetyDistance = Coordinates.distanceToClosestCorner(boardState.getKingPosition());
+
+        moveValue -= kingSafetyDistance * kingDistanceValue;
+        return moveValue;
+    }
+
+    Move generateMuscoviteMove(TablutBoardState boardState) {
+        // bait greedy opponents
+        if (boardState.getTurnNumber() == 0) {
+            return new TablutMove(4, 1, 3, 1, player_id);
+        }
+
+        Map<TablutMove, Integer> moveValues = new HashMap<>();
+
+        // go through player's legal moves
+        for (TablutMove playerMove : boardState.getAllLegalMoves()) {
+            int moveValue = 0;
+            TablutBoardState clonedBoardState = (TablutBoardState) boardState.clone();
+            clonedBoardState.processMove(playerMove);
+
+            // check for win conditions
+            if (clonedBoardState.gameOver()) {
+                if (clonedBoardState.getWinner() == TablutBoardState.MUSCOVITE) {
+                    moveValue = Integer.MAX_VALUE;
+                } else if (clonedBoardState.getWinner() == TablutBoardState.SWEDE) {
+                    moveValue = Integer.MIN_VALUE;
+                }
+
+                moveValues.put(playerMove, moveValue);
+            } else {
+                // go through opponent's moves on following turn
+                for (TablutMove opponentMove : clonedBoardState.getAllLegalMoves()) {
+                    TablutBoardState nextBoardState = (TablutBoardState) clonedBoardState.clone();
+                    nextBoardState.processMove(opponentMove);
+
+                    // check for opponent win condition
+                    if (nextBoardState.gameOver()) {
+                        if (nextBoardState.getWinner() == TablutBoardState.SWEDE) {
+                            moveValue = Integer.MIN_VALUE + 1;
+                            break;
+                        }
+                    }
+                }
+                if (moveValue == Integer.MIN_VALUE + 1) {
+                    moveValues.put(playerMove, moveValue);
+                } else {
+                    moveValue = muscoviteEvalBoard(boardState, clonedBoardState, playerMove);
+                    moveValues.put(playerMove, moveValue);
+                }
+            }
+        }
+        Move myMove = getMaxMove(moveValues);
+        return myMove == null ? boardState.getRandomMove() : myMove;
+    }
+
+    Move generateSwedeMove(TablutBoardState boardState) {
+
+        Map<TablutMove, Integer> moveValues = new HashMap<>();
+
+        // go through player's legal moves
+        for (TablutMove playerMove : boardState.getAllLegalMoves()) {
+            int moveValue = 0;
+            TablutBoardState clonedBoardState = (TablutBoardState) boardState.clone();
+            clonedBoardState.processMove(playerMove);
+
+            // check for win conditions
+            if (clonedBoardState.gameOver()) {
+                if (clonedBoardState.getWinner() == TablutBoardState.SWEDE) {
+                    moveValue = Integer.MAX_VALUE;
+                } else if (clonedBoardState.getWinner() == TablutBoardState.MUSCOVITE) {
+                    moveValue = Integer.MIN_VALUE;
+                }
+
+                moveValues.put(playerMove, moveValue);
+            } else {
+                // go through opponent's moves on following turn
+                for (TablutMove opponentMove : clonedBoardState.getAllLegalMoves()) {
+                    TablutBoardState nextBoardState = (TablutBoardState) clonedBoardState.clone();
+                    nextBoardState.processMove(opponentMove);
+
+                    // check for opponent win condition
+                    if (nextBoardState.gameOver()) {
+                        if (nextBoardState.getWinner() == TablutBoardState.MUSCOVITE) {
+                            moveValue = Integer.MIN_VALUE + 1;
+                            break;
+                        }
+                    }
+                }
+                if (moveValue == Integer.MIN_VALUE + 1) {
+                    moveValues.put(playerMove, moveValue);
+                } else {
+                    moveValue = swedeEvalBoard(clonedBoardState);
+                    moveValues.put(playerMove, moveValue);
+                }
+            }
+        }
+        Move myMove = getMaxMove(moveValues);
+        return myMove == null ? boardState.getRandomMove() : myMove;
+    }
+
+//    private boolean isKingVulnerable(TablutBoardState boardState) {
+//        Coord kingCoord = boardState.getKingPosition();
+//        TablutBoardState.Piece currentPiece = null;
+//        if (kingCoord.equals(Coordinates.get(0,1))) {
+//            for (int j = 2; j < 9; j++) {
+//                currentPiece = boardState.getPieceAt(0, j);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        } else if (kingCoord.equals(Coordinates.get(1,0))) {
+//            for (int i = 2; i < 9; i++) {
+//                currentPiece = boardState.getPieceAt(i, 0);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        } else if (kingCoord.equals(Coordinates.get(8,7))) {
+//            for (int j = 6; j > 0; j--) {
+//                currentPiece = boardState.getPieceAt(8, j);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        } else if (kingCoord.equals(Coordinates.get(7,8))) {
+//            for (int i = 6; i > 0; i--) {
+//                currentPiece = boardState.getPieceAt(i, 8);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        } else if (kingCoord.equals(Coordinates.get(0,7))) {
+//            for (int j = 6; j > 0; j--) {
+//                currentPiece = boardState.getPieceAt(0, j);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        } else if (kingCoord.equals(Coordinates.get(7,0))) {
+//            for (int i = 6; i > 0; i--) {
+//                currentPiece = boardState.getPieceAt(i, 0);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        } else if (kingCoord.equals(Coordinates.get(8,1))) {
+//            for (int j = 2; j < 9; j++) {
+//                currentPiece = boardState.getPieceAt(8, j);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        } else if (kingCoord.equals(Coordinates.get(1,8))) {
+//            for (int i = 2; i < 9; i++) {
+//                currentPiece = boardState.getPieceAt(i, 8);
+//                if (currentPiece == TablutBoardState.Piece.BLACK) {
+//                    return true;
+//                }
+//                else if (currentPiece == TablutBoardState.Piece.WHITE) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+    private Move getMaxMove(Map<TablutMove, Integer> moveValues) {
+        Map.Entry<TablutMove, Integer> maxEntry = null;
+        for (Map.Entry<TablutMove, Integer> entry : moveValues.entrySet()) {
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                maxEntry = entry;
+            }
+        }
+        if (maxEntry != null) {
+            List<TablutMove> bestMoves = new ArrayList<>();
+            for (Map.Entry<TablutMove, Integer> entry : moveValues.entrySet()) {
+                if (entry.getValue().compareTo(maxEntry.getValue()) == 0) {
+                    bestMoves.add(entry.getKey());
+                }
+            }
+            int random = ThreadLocalRandom.current().nextInt(bestMoves.size());
+            return bestMoves.get(random);
+        }
+        return null;
     }
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -1,139 +1,23 @@
 package student_player;
 
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
-
 import boardgame.Move;
-import coordinates.Coord;
-import coordinates.Coordinates;
 import tablut.TablutBoardState;
-import tablut.TablutMove;
 import tablut.TablutPlayer;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends TablutPlayer {
 
-    // todo: add values for vulnerable positions. e.g. lose 1 point for every vulnerable piece
-    // todo: bonus for moving the a piece into a position where it can attach the king
-    // todo: make sure bonus for king distance gives value based on distance improvement on swede turn
-    // todo: decrease value if we're sacrificing a piece
-    // todo: discard move if we put our king in peril
-
-    private final int CAPTURE_VALUE = 10;
-    private final int PIECE_VALUE = 1;
-    private final int KING_DISTANCE_VALUE = 1;
-    private final int VULNERABLE_PIECE_PENALTY = 1;
-    private final int VULNERABLE_KING_PENALTY = 150;
-    private final int CENTER_CAPTURE_BONUS = 40;
-    private final int CORNER_CAPTURE_BONUS = 20;
-    private final Coord CENTER = Coordinates.get(4,4);
-    private final List<Coord> CORNERS = Coordinates.getCorners();
-    private final List<Coord> CENTER_NEIGHBOURS = Coordinates.getNeighbors(CENTER);
+    private final MyTools myTools = new MyTools(player_id);
 
     public StudentPlayer() {
         super("260639146");
     }
 
-    private Move generateMuscoviteMove(TablutBoardState boardState) {
-        if (boardState.getTurnNumber() == 0) {
-            return new TablutMove(4, 1, 3, 1, player_id);
-        }
-
-        int opponent = boardState.getOpponent();
-        List<TablutMove> legalMoves = boardState.getAllLegalMoves();
-        Map<TablutMove, Integer> moveValues = new HashMap<>();
-
-        for (TablutMove legalMove : legalMoves) {
-            int moveValue = 0;
-            TablutBoardState clonedBoardState = (TablutBoardState)boardState.clone();
-            clonedBoardState.processMove(legalMove);
-
-            // check for win conditions
-            // value remains 0 on draw
-            if (clonedBoardState.gameOver()) {
-                if (clonedBoardState.getWinner() == TablutBoardState.MUSCOVITE) {
-                    moveValue = Integer.MAX_VALUE;
-                }
-                else if (clonedBoardState.getWinner() == TablutBoardState.SWEDE) {
-                    moveValue = Integer.MIN_VALUE;
-                }
-            }
-            else {
-                // increase value for each piece owned, decrease for each piece owned by opponent
-                int numPlayerPieces = clonedBoardState.getNumberPlayerPieces(player_id);
-                int numOpponentPieces = clonedBoardState.getNumberPlayerPieces(opponent);
-                moveValue += numPlayerPieces * PIECE_VALUE;
-                moveValue -= numOpponentPieces * PIECE_VALUE;
-
-                // increase value for capturing opponent pieces
-                int numCaptures = numOpponentPieces - boardState.getNumberPlayerPieces(opponent);
-                moveValue += numCaptures * CAPTURE_VALUE;
-
-                // give additional incentive for captures at vulnerable positions
-                // encourages AI to be more aggressive
-                Coord endPosition = legalMove.getEndPosition();
-                for (Coord centerNeighbor : CENTER_NEIGHBOURS) {
-                    if (boardState.isOpponentPieceAt(centerNeighbor)) {
-                        Coord sandwichCoord;
-                        try {
-                            sandwichCoord = Coordinates.getSandwichCoord(CENTER, centerNeighbor);
-                            if (endPosition.equals(sandwichCoord)) {
-                                moveValue += CENTER_CAPTURE_BONUS;
-                            }
-                        } catch (Exception ignored) {}
-                    }
-                }
-                for (Coord corner : CORNERS) {
-                    List<Coord> cornerNeighbours = Coordinates.getNeighbors(corner);
-                    for (Coord cornerNeighbour : cornerNeighbours) {
-                        if (boardState.isOpponentPieceAt(cornerNeighbour)) {
-                            try {
-                                Coord sandwichCoord = Coordinates.getSandwichCoord(corner, cornerNeighbour);
-                                if (endPosition.equals(sandwichCoord)) {
-                                    moveValue += CORNER_CAPTURE_BONUS;
-                                }
-                            } catch (Exception ignored) {}
-                        }
-                    }
-                }
-            }
-            moveValues.put(legalMove, moveValue);
-        }
-        Move myMove = getBestMove(moveValues);
-        return myMove == null ? boardState.getRandomMove() : myMove;
-    }
-
-    // TODO
-    private Move generateSwedeMove(TablutBoardState boardState) {
-        return boardState.getRandomMove();
-    }
-
-    // source: https://stackoverflow.com/a/5911199
-    private Move getBestMove(Map<TablutMove, Integer> moveValues) {
-        Map.Entry<TablutMove, Integer> maxEntry = null;
-        for (Map.Entry<TablutMove, Integer> entry : moveValues.entrySet()) {
-            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
-                maxEntry = entry;
-            }
-        }
-        if (maxEntry != null) {
-            List<TablutMove> bestMoves = new ArrayList<>();
-            for (Map.Entry<TablutMove, Integer> entry : moveValues.entrySet()) {
-                if (entry.getValue().compareTo(maxEntry.getValue()) == 0) {
-                    bestMoves.add(entry.getKey());
-                }
-            }
-            int random = ThreadLocalRandom.current().nextInt(bestMoves.size());
-            return bestMoves.get(random);
-        }
-        return null;
-    }
-
     public Move chooseMove(TablutBoardState boardState) {
         if (player_id == TablutBoardState.MUSCOVITE) {
-            return generateMuscoviteMove(boardState);
+            return myTools.generateMuscoviteMove(boardState);
         } else {
-            return generateSwedeMove(boardState);
+            return myTools.generateSwedeMove(boardState);
         }
     }
 }
