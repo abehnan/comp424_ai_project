1dd41955534b4aeb3706643876e3bf653aab031f
abehnan <abehnan@gmail.com>
2018-04-02 19:29:46 -0400

    improved debug

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -3,6 +3,7 @@ package student_player;
 import java.util.ArrayList;
 import java.util.List;
 
+import boardgame.Board;
 import boardgame.Move;
 import boardgame.Player;
 import tablut.GreedyTablutPlayer;
@@ -16,7 +17,7 @@ import tablut.TablutPlayer;
  */
 public class StudentPlayer extends TablutPlayer {
 
-    private  MyTools myTools;
+    private MyTools myTools;
     private boolean setup = true;
 
     public StudentPlayer() {
@@ -39,11 +40,13 @@ public class StudentPlayer extends TablutPlayer {
     public static void main(String[] args) {
 
 
-        double numGames = 100;
-        int winCount = 0;
+        int numGames = 100;
+        int numDraws = 0;
+        int swedeWins = 0;
+        int muscoviteWins = 0;
         List<Integer> turnCounts = new ArrayList<>();
 
-        for (int i = 1; i <= (int) numGames; i++) {
+        for (int i = 1; i <= numGames; i++) {
             TablutBoardState b = new TablutBoardState();
 
             Player swede = new StudentPlayer();
@@ -64,16 +67,30 @@ public class StudentPlayer extends TablutPlayer {
                 player = (player == muscovite) ? swede : muscovite;
             }
             turnCounts.add(b.getTurnNumber());
-            System.out.println("Game: " + i + " winner: " + TablutMove.getPlayerName(b.getWinner()));
+            System.out.println(
+                    "Game: " + i + " winner: " +
+                            TablutMove.getPlayerName(b.getWinner()) + " in " + b.getTurnNumber() + " turns.");
+            if (b.getWinner() == swede.getColor()) {
+                swedeWins++;
+            }
             if (b.getWinner() == muscovite.getColor()) {
-                winCount++;
+                muscoviteWins++;
+            }
+            if (b.getTurnNumber() == Board.NOBODY) {
+                numDraws++;
             }
         }
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println(numGames + " games");
-        System.out.println(winCount + " wins");
-        double winRate = (double) winCount / numGames * 100;
+        System.out.println(swedeWins + " swede wins");
+        System.out.println(muscoviteWins + " muscovite wins");
+        System.out.println(numDraws + " draws");
+        double swedeWinRate = (double) swedeWins / numGames * 100;
+        double muscoviteWinRate = (double) muscoviteWins / numGames * 100;
         System.out.println("average turn number: " + StudentPlayer.calculateAverage(turnCounts));
-        System.out.println("winrate: " + winRate + "%");
+        System.out.println("swede win rate: " + swedeWinRate + "%");
+        System.out.println("muscovite win rate: " + muscoviteWinRate + "%");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
     }
 
